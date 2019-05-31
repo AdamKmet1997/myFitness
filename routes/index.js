914505e3395fae95cd67efc8636f4b526c136a4f
AdamKmet1997 <adamkmet97@gmail.com>
2019-05-31 22:11:35 +0100

    added path and ejs trainign file

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,3 +9,16 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
+
+// respond with "hello world" when a GET request is made to the homepage
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
+router.get('/training', function (req, res) {
+  res.render('training')
+})
+
+router.get('/plam', function (req, res) {
+  res.render('plan')
+})
